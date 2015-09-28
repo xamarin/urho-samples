56ee4e98894b694765942c34b674a504d9056ef7
EgorBo <egorbo@gmail.com>
2015-09-28 20:28:35 +0300

    Fix runtime error for iOS

--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -2,6 +2,7 @@
 using Foundation;
 using UIKit;
 using Urho.iOS;
+using System.Threading.Tasks;
 
 namespace Urho.Samples.iOS
 {
@@ -11,9 +12,6 @@ namespace Urho.Samples.iOS
 	[Register("AppDelegate")]
 	public partial class AppDelegate : UIApplicationDelegate
 	{
-		// class-level declarations
-		UIWindow window;
-
 		//
 		// This method is invoked when the application has loaded and is ready to run. In this 
 		// method you should instantiate the window, load the UI into it and then make the window
@@ -23,11 +21,14 @@ namespace Urho.Samples.iOS
 		//
 		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
 		{
-			window = new UIWindow(UIScreen.MainScreen.Bounds);
-			window.MakeKeyAndVisible();
-
-			ApplicationLauncher.Run(() => new _23_Water(new Context()));
+			LaunchGame ();
 			return true;
+		}
+
+		public async void LaunchGame()
+		{
+			await Task.Yield ();
+			ApplicationLauncher.Run(() => new _06_SkeletalAnimation(new Context()));
 		}
 	}
 }
