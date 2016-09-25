327fba4cbcae37eff197214d375ace33d14d2d4b
EgorBo <egorbo@gmail.com>
2016-09-25 21:05:29 +0300

    [HoloLens] improve HelloWorld sample

--- HoloLens/HelloWorld/Program.cs
+++ HoloLens/HelloWorld/Program.cs
@@ -51,23 +51,38 @@ namespace HelloWorld
 			// Create a Sphere component which is basically 
 			// a StaticModel with CoreData\Models\Sphere.mdl model
 			var earth = earthNode.CreateComponent<Sphere>();
-			// Usually materials are defined via XML files and 
-			// should be accessed via ResourceCache.GetMaterial(), but 
-			// for our simple case we can use Material.FromImage method
-			earth.SetMaterial(Material.FromImage("Earth.jpg"));
+			// Apply a material (material is a set of tecniques, parameters and textures)
+			earth.SetMaterial(ResourceCache.GetMaterial("Earth.xml"));
+
+			// Let's create a second sphere (slightly bigger - 1.01f) for clouds
+			var cloudsNode = earthNode.CreateChild();
+			cloudsNode.SetScale(1.01f);
+			// Alternative way to create a sphere - create a StaticModel and set Model to Sphere.mdl
+			var clouds = cloudsNode.CreateComponent<StaticModel>();
+			clouds.Model = CoreAssets.Models.Sphere; //same as ResourceCache.GetModel("Models/Sphere.mdl");
+
+			// Create a material _in code_ for the clouds
+			var cloudsMaterial = new Material();
+			cloudsMaterial.SetTechnique(0, CoreAssets.Techniques.DiffAdd, 1, 1);
+			cloudsMaterial.SetTexture(TextureUnit.Diffuse, ResourceCache.GetTexture2D("Earth_Clouds.jpg"));
+			clouds.SetMaterial(cloudsMaterial);
+
+			// By default HoloApplication has a bult-in light (directional)
+			// decrease the brightness
+			DirectionalLight.Brightness -= 0.6f;
 
 			// Same for the Moon
 			var moonNode = earthNode.CreateChild();
 			moonNode.SetScale(0.4f);
 			moonNode.Position = new Vector3(1f, 0, 0);
 			var moon = moonNode.CreateComponent<Sphere>();
+			// Material.FromImage is the easiest way to create a material from an image (using Diff.xml technique)
 			moon.SetMaterial(Material.FromImage("Moon.jpg"));
 
-			// Run a few actions to spin the Earth and the Moon
-			earthNode.RunActions(new RepeatForever(
-				new RotateBy(duration: 1f, deltaAngleX: 0, deltaAngleY: -10, deltaAngleZ: 0)));
-			moonNode.RunActions(new RepeatForever(
-				new RotateAroundBy(1f, earthNode.WorldPosition, 0, -10, 0)));
+			// Run a few actions to spin the Earth, the Moon and the clouds.
+			earthNode.RunActions(new RepeatForever(new RotateBy(duration: 1f, deltaAngleX: 0, deltaAngleY: -4, deltaAngleZ: 0)));
+			cloudsNode.RunActions(new RepeatForever(new RotateBy(duration: 1f, deltaAngleX: 0, deltaAngleY: 1, deltaAngleZ: 0)));
+			moonNode.RunActions(new RepeatForever(new RotateAroundBy(1f, earthNode.WorldPosition, 0, -3, 0)));
 		}
 
 		protected override void OnUpdate(float timeStep)
